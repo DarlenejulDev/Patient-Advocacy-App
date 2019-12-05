0b541e6dffb13881a1b32afc068ade3a6c249b4e
Darlene Julien <darlenejuliendev@gmail.com>
2019-12-05 09:29:50 -0500

    adding graph

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,119 +1,30 @@
 
-let painLocation = document.querySelectorAll('.painLocation')
-
+let painLocation = document.getElementById('painLocation')
+let feelingsReasoning = document.getElementById('feelingsReasoning')
 let extraInfo= document.querySelectorAll('.extraInfo')
 
 let del = document.querySelectorAll('.delete')
-// let update= document.getElementsByClassName('update')
+
 let update =document.querySelectorAll('.update')
 
 
-let numberInput=document.querySelectorAll('.number')
-
-let textInput=document.getElementById('msg')
-
 let btn = document.getElementById('button')
+let painQuestionOption = document.getElementById('painQuestionOption')
+
 console.log(btn);
-// let find= document.getElementsByClassName('find')
-//
-// let mood = document.getElementById('mood')
-// let moodInherited = document.getElementById('moodInherited')
-
-
-// mood.addEventListener('change', ()=>{
-//   var userMood = mood.options[mood.selectedIndex].value;
-//   moodInherited.textContent = userMood
-// })
-
-// btn.addEventListener('click',send,false);
-//
-// function send(){
-//   const number = numberInput.value.replace(/\D/g, '');
-//   const text= textInput.value;
-//   fetch('/mood'),{
-//     method: 'post',
-//     headers: {
-//       'Content-type':'application/json'
-//     },
-//     body:JSON.stringify{
-//       ({number: number,text:text})}
-//   })
-//   .then(function(res){})
-//   console.log(res);
-//   })
-//   .catch(function(err) {
-//    console.log(err);
-//    });
-//    }
 
 
 
-
-
-
-// Any user w/o accounts
-// Array.from(find).forEach(function(element) {
-//       element.addEventListener('click', function(){
-//         const userWord = this.parentNode.parentNode.childNodes[1].innerText
-//         const userMeaning = this.parentNode.parentNode.childNodes[3].innerText
-//         fetch('profileEntry', {
-//           method: 'put',
-//           headers: {'Content-Type': 'application/json'},
-//           body: JSON.stringify({
-//             'userWord': userWord,
-//             'userMeaning': userMeaning,
-//
-//           })
-//         })
-//         .then(response => {
-//           if (response.ok) return response.json()
-//         })
-//         .then(data => {
-//           console.log(data)
-//           // window.location.reload(true)
-//         })
-//       });
-// });
-
-
-// Array.from(update).forEach(function(element) {
-//       element.addEventListener('click', function(){
-//         console.log('I clicked')
-//
-//       //     var txt;
-//       //     if (confirm("Are you sure you would like to update this?")) {
-//       //       txt = "Update was successful!";
-//       //     } else {
-//       //       txt = "No changes made!";
-//       //     }
-//       // document.getElementById("demo").innerHTML = txt;
-// // })
-//         const painLocation =
-//         this.parentNode.childNodes[1].childNodes[0].innerText
-//          // this.parentNode.childNodes[9].childNodes[1].childNodes[0]
-//         const extraInfo =
-//         this.parentNode.childNodes[13].childNodes[1].childNodes[0].innerText
-//         fetch('/mood', {
-//           method: 'put',
-//           headers: {'Content-Type': 'application/json','Accept': 'application/json'},
-//           body: JSON.stringify({
-//             'painLocation':painLocation,
-//             'extraInfo': extraInfo
-//
-//           })
-//         })
-//         .then(response => {
-//           if (response.ok) return response.json()
-//         })
-//       });
-// });
 for (let i=0; i< update.length; i++){
-  update[i].addEventListener('click',( )=>{
+  update[i].addEventListener('click',(event)=>{
     alert('I work');
+            // let body = {}
+            // body[event.target.name] = event.target.innerText;
             fetch('userEntries', {
               method: 'put',
               headers: {'Content-Type': 'application/json','Accept': 'application/json'},
               body: JSON.stringify({
+
                 'painLocation':painLocation[i].innerText,
                 'extraInfo': extraInfo[i].innerHTML
 
@@ -124,6 +35,25 @@ for (let i=0; i< update.length; i++){
             })
           });
     }
+
+// painQuestionOption.addEventListener('change', (event) => {
+  // event.target & event.currentTarget
+  // event.target.value === "Yes I do", "No I don't"
+//   console.log(event)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //   })
